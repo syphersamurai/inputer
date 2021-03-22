db012afcb8fcd21201f780a1f86835d512914bb8
syphersamurai <syphersamurai@gmail.com>
2021-03-22 01:44:51 +0100

    inputer initalized

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# inputer
-vscode extension for transforming text cases
+
+# Inputer
+
+**Inputer** Let you Easily convert text between different letter cases: lower case, UPPER CASE, Sentence Case.
+
+![Inputer Gif](https://syphersamurai.github.io/inputer/images/Screenshot/inputer.gif)
+
+
+
+* `inputer: UPPER CASE` =>  transforms any text to uppercase case.
+* `inputer: LOWER CASE` =>  transforms any text to lowercase case.
+* `inputer: SENTENCE CASE` =>  transforms any text to sentence case.
+
+
+
+### Keyboard Shortcuts 
+
+
+
+
+* `inputer: UPPER CASE` =>   (<kbd>ALT</kbd> + <kbd>I</kbd> ,   <kbd>ALT</kbd> <kbd>U</kbd>).
+* `inputer: LOWER CASE` =>  (<kbd>ALT</kbd> + <kbd>I</kbd> ,   <kbd>ALT</kbd> <kbd>L</kbd>).
+* `inputer: SENTENCE CASE` =>   (<kbd>ALT</kbd> + <kbd>I</kbd> ,   <kbd>ALT</kbd> <kbd>S</kbd>).
+
+
+
+
+### For more information
+
+* [Inputer Github Repo](https://github.com/syphersamurai/inputer)
+
+### Issues
+* [Inputer Github Repo](https://github.com/syphersamurai/inputer/issues)
+
+
+License
+----
+
+ Apache-2.0 License 
+
+**Enjoy! 😊**
---
+++ extension.js
@@ -0,0 +1,79 @@
+
+const vscode = require('vscode')
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "inputer" is now active!');
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("inputer.toUpperCase", async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("editor does not exist!");
+        return;
+      }
+
+      const text = editor.document.getText(editor.selection);
+      const toUpperCase = text.toUpperCase();
+      editor.edit((edit) => {
+        edit.replace(editor.selection, toUpperCase);
+      });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("inputer.toLowerCase", async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("editor does not exist!");
+        return;
+      }
+
+      const text = editor.document.getText(editor.selection);
+      const toUpperCase = text.toLowerCase();
+      editor.edit((edit) => {
+        edit.replace(editor.selection, toUpperCase);
+      });
+
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("inputer.toSentenceCase", async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("editor does not exist!");
+        return;
+      }
+
+      const text = editor.document.getText(editor.selection);
+
+      function CapitalizedCaseFunction(str) {
+        var splitStr = str.toLowerCase().split(" ");
+        for (var i = 0; i < splitStr.length; i++) {
+          // You do not need to check if i is larger than splitStr length, as your for does that for you
+          // Assign it back to the array
+          splitStr[i] =
+            splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        }
+        // Directly return the joined string
+        return splitStr.join(" ");
+      }
+      let toCapitalizedCase = CapitalizedCaseFunction(text);
+      editor.edit((edit) => {
+        edit.replace(editor.selection, toCapitalizedCase);
+      });
+    })
+  );
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
